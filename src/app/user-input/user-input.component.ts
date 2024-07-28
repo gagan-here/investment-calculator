a2fd9231f62b8847818141511fcf99304f691455
Gagan Basnet <chatwidmeah@gmail.com>
2024-07-29 00:32:41 +0545

    added cross-component communication of user-input component with app component

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,12 +9,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number,
+    duration: number,
+    expectedReturn: number,
+    annualInvestment: number
+  }>();
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enterdExpectedReturn = '5';
   enteredDuration = '10';
 
   onSubmit() {
-    
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enterdExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment
+    })
   }
 }
